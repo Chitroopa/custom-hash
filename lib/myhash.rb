070e354c9a1001859585b0a6652b26e1deb55d17
Anatoliy Klots and Chitroopa Manikkavasagam <anatoliy@>
2017-04-24 16:12:36 -0700

    Add loogic for store,fetch and length

--- lib/myhash.rb
+++ lib/myhash.rb
@@ -1,3 +1,4 @@
+require('pry')
 class MyHash
   define_method(:initialize) do
     @my_key_array = []
@@ -13,4 +14,23 @@ class MyHash
     index = @my_key_array.index(key)
     @my_value_array[index]
   end
+
+  define_method(:has_key?) do |key|
+    @my_key_array.include?(key)
+  end
+
+  define_method(:has_value?) do |value|
+    @my_value_array.include?(value)
+  end
+
+  define_method(:length) do
+    @my_key_array.length
+  end
+
+#   define_method(:merge) do |merge_array|
+# binding.pry
+#     @my_key_array + another_array
+#     @my_value_array + another_array
+#   end
+
 end
--- spec/myhash_spec.rb
+++ spec/myhash_spec.rb
@@ -14,10 +14,39 @@ describe(MyHash) do
     it("returns true if the key is present") do
       test_hash = MyHash.new()
       test_hash.myStore("kitten", "cute")
-      
-
-
-      expect(test_hash.myFetch("kitten")).to(eq("cute"))
+      test_hash.myStore("dog", "bark")
+      expect(test_hash.has_key?("kitten")).to(eq(true))
     end
   end
+
+  describe("#has_value?") do
+    it("returns true if the value is present") do
+      test_hash = MyHash.new()
+      test_hash.myStore("kitten", "cute")
+      test_hash.myStore("dog", "bark")
+      expect(test_hash.has_value?("cute")).to(eq(true))
+    end
+  end
+
+  describe("#length") do
+    it("returns length of MyHash") do
+      test_hash = MyHash.new()
+      test_hash.myStore("kitten", "cute")
+      test_hash.myStore("dog", "bark")
+      expect(test_hash.length()).to(eq(2))
+    end
+  end
+
+  # describe("#merge") do
+  #   it("returns merge of 2 arrays") do
+  #     test_hash_1 = MyHash.new()
+  #     test_hash_2 = MyHash.new()
+  #     test_hash_1.myStore("kitten", "cute")
+  #     test_hash_1.myStore("dog", "bark")
+  #
+  #     test_hash_2.myStore("snake", "hiss")
+  #     test_hash_2.myStore("bird", "sing")
+  #     expect(test_hash_1.merge(test_hash_2)).to(eq())
+  #   end
+  # end
 end
